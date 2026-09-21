5e072098d2b49f76d93ed181d2ecf7bd8c928968
Viktoria Sharifullina <viktoriasharifullina@gmail.com>
2026-09-21 12:24:50 +0300

    Сохранение введенных значений

--- app/public/admin/js/property.js
+++ app/public/admin/js/property.js
@@ -1,10 +1,79 @@
 (function ($) {
     'use strict';
 
+    function collectPropertyValues() {
+        var values = {};
+
+        $('.name_select_rielt').each(function () {
+            var $field = $(this);
+            var propertyId = $field.attr('data-property');
+            var $input = $field.find('input.ag_pole_good').first();
+            var $select = $field.find('select.ag_pole_good').first();
+
+            if ($select.length) {
+                values[propertyId] = {
+                    type: 'select',
+                    value: $select.val()
+                };
+                return;
+            }
+
+            if ($input.length) {
+                values[propertyId] = {
+                    type: 'input',
+                    value: $input.val()
+                };
+                return;
+            }
+
+            var checked = [];
+            $field.find('input[type="checkbox"]').each(function () {
+                if ($(this).prop('checked')) {
+                    checked[checked.length] = $(this).siblings('.ckeck_param').attr('data-val');
+                }
+            });
+
+            values[propertyId] = {
+                type: 'checkbox',
+                value: checked
+            };
+        });
+
+        return values;
+    }
+
+    function restorePropertyValues($properties, values) {
+        $.each(values, function (propertyId, property) {
+            var $field = $properties.find('.name_select_rielt').filter(function () {
+                return $(this).attr('data-property') === propertyId;
+            });
+
+            if (!$field.length) {
+                return;
+            }
+
+            if (property.type === 'select') {
+                $field.find('select.ag_pole_good').val(property.value);
+                return;
+            }
+
+            if (property.type === 'input') {
+                $field.find('input.ag_pole_good').val(property.value);
+                return;
+            }
+
+            $field.find('input[type="checkbox"]').each(function () {
+                var value = $(this).siblings('.ckeck_param').attr('data-val');
+                $(this).prop('checked', $.inArray(value, property.value) !== -1);
+            });
+        });
+    }
+
     // Выбор категории в товаре и обновление блока характеристик.
     $('body').on('change', '.js-category', function () {
         var category = [];
         var $properties = $('.property_all');
+        var propertyValues = collectPropertyValues();
 
         $(this).closest('.add_good_name_category')
             .toggleClass('category_checked is-selected', this.checked);
@@ -20,11 +89,12 @@
             url: './admin/ajax/property/Refresh_Property_Good.php',
             dataType: 'html',
             data: { category: category },
-            success: function (data) {
-                if (data != 'no') {
-                    $properties.html(data);
-                }
-            },
+        success: function (data) {
+            if (data != 'no') {
+                $properties.html(data);
+                restorePropertyValues($properties, propertyValues);
+            }
+        },
             error: function () {
                 $properties.html('<div class="error-state">Не удалось обновить характеристики.</div>');
             },
